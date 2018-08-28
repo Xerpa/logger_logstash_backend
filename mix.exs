4b5822057db13796390a56353a26d56abeabb8d8
Thales Macedo Garitezi <tgaritezi@xerpa.com.br>
2018-08-28 14:47:03 -0300

    Ease the requirement on Timex version.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule LoggerLogstashBackend.Mixfile do
      name: "logger_logstash_backend",
      source_url: "https://github.com/marcelog/logger_logstash_backend",
      version: "5.0.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.6",
      description: description(),
      package: package(),
      deps: deps()]
@@ -21,7 +21,7 @@ defmodule LoggerLogstashBackend.Mixfile do
       {:earmark, "~> 1.0.3", only: :dev},
       {:ex_doc, "~> 0.14.5", only: :dev},
       {:exjsx, "~> 3.2.1"},
-      {:timex, "~> 3.1.8"}
+      {:timex, ">= 3.0.0"}
     ]
   end
 
